f1be5ef39ba231e095d3eaea96e417646c4450f5
Gary Verhaegen <gary.verhaegen@style.com>
2017-10-11 14:09:46 +0100

    remove AgedBrie special cases

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -30,7 +30,7 @@ const Sulfuras = (sellIn) => {
 };
 
 const BackstagePasses = (bandName, sellIn, quality) => {
-    return new Item("Backstage passes to a " + bandName + " concert", sellIn, quality);
+  return new Item("Backstage passes to a " + bandName + " concert", sellIn, quality);
 };
 
 class Shop {
@@ -44,7 +44,7 @@ class Shop {
         this.items[i] = this.items[i].tick();
         continue;
       };
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           this.items[i].quality = this.items[i].quality - 1;
         }
@@ -67,18 +67,12 @@ class Shop {
       }
       this.items[i].sellIn = this.items[i].sellIn - 1;
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              this.items[i].quality = this.items[i].quality - 1;
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+        if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.items[i].quality > 0) {
+            this.items[i].quality = this.items[i].quality - 1;
           }
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
+          this.items[i].quality = this.items[i].quality - this.items[i].quality;
         }
       }
     }
